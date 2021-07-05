0d73564a623130ba8aded845b0e9a5c5f8408460
dinnibela <dinnibela@gmail.com>
2021-07-04 21:55:28 -0400

    Compilador

---
+++ Compilador/Compilador/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿
+namespace Compilador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.richRepetir = new System.Windows.Forms.RichTextBox();
+            this.richSi = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.CCCode = new System.Windows.Forms.RichTextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.analisisSin = new System.Windows.Forms.RichTextBox();
+            this.Ccode = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richsemantica = new System.Windows.Forms.RichTextBox();
+            this.RIchTxLexico = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CodigoMeedCompyTxt = new System.Windows.Forms.RichTextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.BCargar = new System.Windows.Forms.Button();
+            this.BAnalisis = new System.Windows.Forms.Button();
+            this.BTraducir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox10.Location = new System.Drawing.Point(-48, -38);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(22, 27);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox10.TabIndex = 84;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox9.Location = new System.Drawing.Point(-82, -36);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(22, 20);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox9.TabIndex = 83;
+            this.pictureBox9.TabStop = false;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Location = new System.Drawing.Point(1055, 522);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(76, 13);
+            this.label12.TabIndex = 98;
+            this.label12.Text = "Analisis Léxico";
+            // 
+            // richRepetir
+            // 
+            this.richRepetir.Location = new System.Drawing.Point(764, 411);
+            this.richRepetir.Name = "richRepetir";
+            this.richRepetir.Size = new System.Drawing.Size(89, 23);
+            this.richRepetir.TabIndex = 96;
+            this.richRepetir.Text = "";
+            this.richRepetir.Visible = false;
+            // 
+            // richSi
+            // 
+            this.richSi.Location = new System.Drawing.Point(901, 414);
+            this.richSi.Name = "richSi";
+            this.richSi.Size = new System.Drawing.Size(94, 23);
+            this.richSi.TabIndex = 95;
+            this.richSi.Text = "";
+            this.richSi.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Location = new System.Drawing.Point(-70, 521);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 13);
+            this.label8.TabIndex = 91;
+            this.label8.Text = "Variables #";
+            // 
+            // CCCode
+            // 
+            this.CCCode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.CCCode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.CCCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CCCode.ForeColor = System.Drawing.SystemColors.Info;
+            this.CCCode.Location = new System.Drawing.Point(590, 39);
+            this.CCCode.Name = "CCCode";
+            this.CCCode.Size = new System.Drawing.Size(422, 346);
+            this.CCCode.TabIndex = 82;
+            this.CCCode.Text = "";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(-70, 11);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1204, 10);
+            this.panel3.TabIndex = 76;
+            // 
+            // analisisSin
+            // 
+            this.analisisSin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(94)))));
+            this.analisisSin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.analisisSin.ForeColor = System.Drawing.SystemColors.Info;
+            this.analisisSin.Location = new System.Drawing.Point(12, 443);
+            this.analisisSin.Name = "analisisSin";
+            this.analisisSin.Size = new System.Drawing.Size(251, 110);
+            this.analisisSin.TabIndex = 63;
+            this.analisisSin.Text = "";
+            // 
+            // Ccode
+            // 
+            this.Ccode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Ccode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Ccode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(74)))), ((int)(((byte)(64)))));
+            this.Ccode.Location = new System.Drawing.Point(547, 14);
+            this.Ccode.Name = "Ccode";
+            this.Ccode.ReadOnly = true;
+            this.Ccode.Size = new System.Drawing.Size(473, 387);
+            this.Ccode.TabIndex = 79;
+            this.Ccode.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(693, 417);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 72;
+            this.label4.Text = "Analisis Semántico";
+            // 
+            // richsemantica
+            // 
+            this.richsemantica.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(94)))));
+            this.richsemantica.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richsemantica.ForeColor = System.Drawing.SystemColors.Info;
+            this.richsemantica.Location = new System.Drawing.Point(667, 440);
+            this.richsemantica.Name = "richsemantica";
+            this.richsemantica.Size = new System.Drawing.Size(296, 113);
+            this.richsemantica.TabIndex = 71;
+            this.richsemantica.Text = "";
+            // 
+            // RIchTxLexico
+            // 
+            this.RIchTxLexico.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(94)))));
+            this.RIchTxLexico.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.RIchTxLexico.ForeColor = System.Drawing.SystemColors.Info;
+            this.RIchTxLexico.Location = new System.Drawing.Point(316, 443);
+            this.RIchTxLexico.Name = "RIchTxLexico";
+            this.RIchTxLexico.Size = new System.Drawing.Size(310, 110);
+            this.RIchTxLexico.TabIndex = 66;
+            this.RIchTxLexico.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(76, 414);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 67;
+            this.label2.Text = "Analisis Sintáctico";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(761, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 64;
+            this.label1.Text = "Analisis Léxico";
+            // 
+            // CodigoMeedCompyTxt
+            // 
+            this.CodigoMeedCompyTxt.AutoWordSelection = true;
+            this.CodigoMeedCompyTxt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.CodigoMeedCompyTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.CodigoMeedCompyTxt.EnableAutoDragDrop = true;
+            this.CodigoMeedCompyTxt.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CodigoMeedCompyTxt.ForeColor = System.Drawing.SystemColors.Info;
+            this.CodigoMeedCompyTxt.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.CodigoMeedCompyTxt.Location = new System.Drawing.Point(-10, 39);
+            this.CodigoMeedCompyTxt.Name = "CodigoMeedCompyTxt";
+            this.CodigoMeedCompyTxt.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.CodigoMeedCompyTxt.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.CodigoMeedCompyTxt.ShowSelectionMargin = true;
+            this.CodigoMeedCompyTxt.Size = new System.Drawing.Size(435, 346);
+            this.CodigoMeedCompyTxt.TabIndex = 62;
+            this.CodigoMeedCompyTxt.TabStop = false;
+            this.CodigoMeedCompyTxt.Text = "Compilador Dinnibel";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(64)))));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(-70, 22);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(527, 386);
+            this.panel2.TabIndex = 75;
+            // 
+            // BCargar
+            // 
+            this.BCargar.Location = new System.Drawing.Point(463, 59);
+            this.BCargar.Name = "BCargar";
+            this.BCargar.Size = new System.Drawing.Size(75, 23);
+            this.BCargar.TabIndex = 99;
+            this.BCargar.Text = "Cargar Codigo";
+            this.BCargar.UseVisualStyleBackColor = true;
+            this.BCargar.Click += new System.EventHandler(this.BCargar_Click);
+            // 
+            // BAnalisis
+            // 
+            this.BAnalisis.Location = new System.Drawing.Point(463, 88);
+            this.BAnalisis.Name = "BAnalisis";
+            this.BAnalisis.Size = new System.Drawing.Size(75, 23);
+            this.BAnalisis.TabIndex = 100;
+            this.BAnalisis.Text = "Analizar";
+            this.BAnalisis.UseVisualStyleBackColor = true;
+            this.BAnalisis.Click += new System.EventHandler(this.BAnalisis_Click);
+            // 
+            // BTraducir
+            // 
+            this.BTraducir.Location = new System.Drawing.Point(463, 117);
+            this.BTraducir.Name = "BTraducir";
+            this.BTraducir.Size = new System.Drawing.Size(75, 23);
+            this.BTraducir.TabIndex = 101;
+            this.BTraducir.Text = "Traducir";
+            this.BTraducir.UseVisualStyleBackColor = true;
+            this.BTraducir.Visible = false;
+            this.BTraducir.Click += new System.EventHandler(this.BTraducir_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(349, 417);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 102;
+            this.label3.Text = "Analisis Lexico";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 559);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BTraducir);
+            this.Controls.Add(this.BAnalisis);
+            this.Controls.Add(this.BCargar);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.richRepetir);
+            this.Controls.Add(this.richSi);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.CCCode);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.analisisSin);
+            this.Controls.Add(this.Ccode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richsemantica);
+            this.Controls.Add(this.RIchTxLexico);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CodigoMeedCompyTxt);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.RichTextBox richRepetir;
+        private System.Windows.Forms.RichTextBox richSi;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RichTextBox CCCode;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.RichTextBox analisisSin;
+        private System.Windows.Forms.RichTextBox Ccode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richsemantica;
+        private System.Windows.Forms.RichTextBox RIchTxLexico;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox CodigoMeedCompyTxt;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button BCargar;
+        private System.Windows.Forms.Button BAnalisis;
+        private System.Windows.Forms.Button BTraducir;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
